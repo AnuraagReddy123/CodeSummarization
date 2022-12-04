4d3b157d7169c2511496ef56a3721ff581a9bbd4
AASHRITH1903 <cs19b025@iittp.ac.in>
2022-12-04 19:48:57 +0530

    batched the test set

--- Model_Pytorch/Predict.py
+++ Model_Pytorch/Predict.py
@@ -26,13 +26,9 @@ if __name__=='__main__':
 
     dataset_test = load_data(os.path.join('Data', 'dataset_train.json'))
 
-    batch_pr, batch_prdesc_shift, batch_prdesc = next(generate_batch(dataset_test, n_points))
-
     model = Model(Constants.VOCAB_SIZE, Constants.HIDDEN_DIM, Constants.EMBEDDING_DIM, num_layers=Constants.NUM_LAYERS).to(device)
     model = nn.DataParallel(model)
     model.load_state_dict(torch.load('Model_Pytorch/model_best_train.pt'))
-
-    pred_batch_prdesc = model.module.predict(batch_pr, Constants.MAX_LEN)
 
     with open('Data/vocab.txt', 'r') as f:
         vocab = eval(f.read())
@@ -46,27 +42,31 @@ if __name__=='__main__':
     rouge_2_total = 0.0
     rouge_l_total = 0.0
 
-    for i in range(len(batch_pr)):
+    for (batch_pr, batch_prdesc_shift, batch_prdesc) in generate_batch(dataset_test, Constants.BATCH_SIZE):
 
-        gt = tensor_to_text(batch_prdesc[i], vocab)
-        pred = tensor_to_text(pred_batch_prdesc[i], vocab)
-        # Take only uptill the END token
-        gt1 = gt.split('<END>')[0].strip().split() + ['<END>']
-        pred1 = pred.split('<END>')[0].strip().split() + ['<END>']
+        pred_batch_prdesc = model.module.predict(batch_pr, Constants.MAX_LEN)
 
-        bleu = bleu4(gt1, pred1)
-        r_score = r.get_scores(' '.join(pred1), ' '.join(gt1))[0]
+        for i in range(len(batch_pr)):
 
-        print(f"Ground Truth:\n{gt}\n\nPrediction:\n{pred}\n")
-        print(f"Bleu: {bleu}\nRouge-1: {r_score['rouge-1']['f']}\nRouge-2: {r_score['rouge-2']['f']}\nRouge-L: {r_score['rouge-l']['f']}\n\n--------------------\n\n")
+            gt = tensor_to_text(batch_prdesc[i], vocab)
+            pred = tensor_to_text(pred_batch_prdesc[i], vocab)
+            # Take only uptill the END token
+            gt1 = gt.split('<END>')[0].strip().split() + ['<END>']
+            pred1 = pred.split('<END>')[0].strip().split() + ['<END>']
 
-        bleu_total += bleu
-        rouge_1_total += r_score['rouge-1']['f']
-        rouge_2_total += r_score['rouge-2']['f']
-        rouge_l_total += r_score['rouge-l']['f']
+            bleu = bleu4(gt1, pred1)
+            r_score = r.get_scores(' '.join(pred1), ' '.join(gt1))[0]
+
+            print(f"Ground Truth:\n{gt}\n\nPrediction:\n{pred}\n")
+            print(f"Bleu: {bleu}\nRouge-1: {r_score['rouge-1']['f']}\nRouge-2: {r_score['rouge-2']['f']}\nRouge-L: {r_score['rouge-l']['f']}\n\n--------------------\n\n")
+
+            bleu_total += bleu
+            rouge_1_total += r_score['rouge-1']['f']
+            rouge_2_total += r_score['rouge-2']['f']
+            rouge_l_total += r_score['rouge-l']['f']
     
-    bleu_total /= len(batch_pr)
-    rouge_1_total /= len(batch_pr)
-    rouge_2_total /= len(batch_pr)
-    rouge_l_total /= len(batch_pr)
+    bleu_total /= len(dataset_test)
+    rouge_1_total /= len(dataset_test)
+    rouge_2_total /= len(dataset_test)
+    rouge_l_total /= len(dataset_test)
     print(f"Total Avg Results:\n\nBleu: {bleu_total}\nRouge-1: {rouge_1_total}\nRouge-2: {rouge_2_total}\nRouge-L: {rouge_l_total}\n")
